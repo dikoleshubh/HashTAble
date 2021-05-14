fff66ed24a467987f2b70eeaf6d09d389092763b
dikoleshubh <dikole.shubh@gmail.com>
2021-05-14 19:38:42 +0530

    Has Table

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HAsh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        }
+    }
+}
